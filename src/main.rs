419a7d3e3b4701478e7344d6b729177f4572980d
Ty Overby <ty@pre-alpha.com>
2016-12-16 13:26:19 -0800

    Finish polygon testing

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,37 @@ fn run(program: &str) {
     let kernel = proq.create_kernel("apply").unwrap();
 
     kernel.arg_buf(&buf).arg_scl(DIM).enq().unwrap();
-    let mut vec = vec![5.0f32; buf.len()];
+    let mut vec = vec![0.0f32; buf.len()];
     buf.read(&mut vec).enq().unwrap();
 
     save_image(&vec, DIM, "out.png", ColorMode::Debug);
 }
 
 fn main() {
+    /*
     let scene = circle(50.0, 50.0, 20.0).and(&circle(60.0, 60.0, 20.0));
 
     let program  = compile(&scene);
     println!("{}", program);
     run(&program);
+    */
+
+    let mut xs = Vec::new();
+    let mut ys = Vec::new();
+    let r = 20.0;
+    let pi = 3.14159;
+    let n = 100;
+
+    for x in 0 .. n {
+        let x = x as f32;
+        let n = n as f32;
+        xs.push(f32::cos(2.0 * pi / n * x) * r + 50.0);
+        ys.push(f32::sin(2.0 * pi / n * x) * r + 50.0);
+    }
+
+    let buffer = implicit_gpu::polygon::run_poly(&xs, &ys);
+    let mut vec = vec![0.0f32; buffer.len()];
+    buffer.read(&mut vec).enq().unwrap();
+
+    save_image(&vec, DIM, "out-poly.png", ColorMode::Debug);
 }
--- src/polygon/mod.rs
+++ src/polygon/mod.rs
@@ -1,7 +1,43 @@
 use ocl::{ProQue, Buffer};
 
 const PROGRAM: &'static str = r#"
-__kernel void apply(__global float* buffer, size_t width) {
+float dist_to_point(float x1, float y1, float x2, float y2) {
+    float dx = x1 - x2;
+    float dy = y1 - y2;
+    return sqrt(dx * dx + dy * dy);
+}
+
+float dist_to_line(float x, float y, float p1x, float p1y, float p2x, float p2y) {
+    float l2 = dist_to_point(p1x, p1y, p2x, p2y);
+    if (l2 == 0.0) {
+        return dist_to_point(x, y, p1x, p1y);
+    }
+
+    float temp1 = p2y - p1y;
+    float temp2 = p2x - p1x;
+    float temp3 = y - p1y;
+    float temp4 = x - p1x;
+
+    float t = (temp4 * temp2 + temp3 * temp1) / l2;
+    float s =  temp2 * temp3 - temp4 * temp1;
+
+    float invert = -1.0;
+    if (s < 0.0) {
+        invert = 1.0;
+    }
+
+    if (t < 0.0) {
+        return dist_to_point(x, y, p1x, p1y) * invert;
+    } else if (t > 1.0) {
+        return dist_to_point(x, y, p2x, p2y) * invert;
+    } else {
+        float npx = p1x + t * (p2x - p1x);
+        float npy = p1y + t * (p2y - p1y);
+        return dist_to_point(x, y, npx, npy) * invert;
+    }
+}
+
+__kernel void apply(__global float* buffer, size_t width, __global float* xs, __global float* ys, size_t count) {
     size_t x = get_global_id(0);
     size_t y = get_global_id(1);
     size_t pos = x + y * width;
@@ -9,21 +45,48 @@ __kernel void apply(__global float* buffer, size_t width) {
     float x_s = (float) x;
     float y_s = (float) y;
 
-    float minimum = INFINITY;
+    float minimum = dist_to_line(
+        x_s, y_s,
+        xs[count - 1], ys[count-1],
+        xs[0], ys[0]);
 
+    for(size_t i = 0; i < count - 1; i++) {
+        float new = dist_to_line(
+            x_s, y_s,
+            xs[i], ys[i],
+            xs[i + 1], ys[i + 1]);
+        float new_abs = fabs(new);
+        float min_abs = fabs(minimum);
+        if (new_abs < min_abs) {
+            minimum = new;
+        }
+    }
 
     buffer[pos] = minimum;
 }
 "#;
 
-const DIM: usize = 256;
+const DIM: usize = 100;
 
-pub fn run_poly(points: &[f32]) -> Buffer<f32> {
-    let proq = ProQue::builder().src(PROGRAM).dims([DIM, DIM]).build().unwrap();
+pub fn run_poly(xs: &[f32], ys: &[f32], width: usize, height: usize) -> Buffer<f32> {
+    assert_eq!(xs.len(), ys.len());
+    let len = xs.len();
+
+    let proq = ProQue::builder().src(PROGRAM).dims([width, height]).build().unwrap();
     let buf = proq.create_buffer::<f32>().unwrap();
     let kernel = proq.create_kernel("apply").unwrap();
 
-    kernel.arg_buf(&buf).arg_scl(DIM).enq().unwrap();
+    let copy = ::ocl::flags::MEM_COPY_HOST_PTR;
+    let xs_buf = Buffer::new(proq.queue(), Some(copy), [len], Some(xs)).unwrap();
+    let ys_buf = Buffer::new(proq.queue(), Some(copy), [len], Some(ys)).unwrap();
+
+    kernel
+        .arg_buf(&buf)
+        .arg_scl(DIM)
+        .arg_buf(&xs_buf)
+        .arg_buf(&ys_buf)
+        .arg_scl(len)
+        .enq().unwrap();
 
     buf
 }
