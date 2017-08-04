4121ae0b7ddfc02d03456a27247f23b61d67c7dd
Ty Overby <ty@pre-alpha.com>
2017-08-04 16:58:25 -0700

    clean deps

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,9 @@ extern crate combine;
 extern crate serde_json;
 extern crate hyper;
 extern crate futures;
-extern crate hyper_router;
 
 pub mod api;
 pub mod routes;
 pub mod parse_route;
 pub mod static_file;
-
 pub fn create() -> api::Server { api::Server::new() }
