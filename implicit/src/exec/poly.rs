d46bab45dd2f8939f4399c931510e16bd83c3fde
Ty Overby <ty@pre-alpha.com>
2018-08-18 17:07:19 -0700

    add bad test for a line

--- implicit/src/exec/poly.rs
+++ implicit/src/exec/poly.rs
@@ -41,3 +41,28 @@ expectation_test!{
         save_field_buffer(&buffer, w_bw, ColorMode::BlackAndWhite);
     }
 }
+
+expectation_test!{
+    fn expectation_test_exec_line_BAD(mut provider: Provider) {
+        use ocaml::*;
+        use euclid::*;
+
+        let ctx = OpenClContext::default();
+        let polygon = Polygon {
+            points: vec![
+                point2(1.0, 1.0),
+                point2(15.0, 15.0),
+
+                point2(15.0, 15.0),
+                point2(1.0, 1.0),
+            ],
+            mat: Transform2D::identity(),
+        };
+        let buffer = exec_poly(&ctx, polygon, 20, 20);
+
+        let w_color = provider.png_writer("out.color.png");
+        let w_bw = provider.png_writer("out.bw.png");
+        save_field_buffer(&buffer, w_color, ColorMode::Debug);
+        save_field_buffer(&buffer, w_bw, ColorMode::BlackAndWhite);
+    }
+}
